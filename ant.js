053f6c5ff151dc282d01c81f04b6ba4b9e9651da
Carlos Moya <mrcamoga@gmail.com>
2022-08-03 17:47:29 +0200

    Update ant.js

--- ant.js
+++ ant.js
@@ -1,43 +1,60 @@
 function getDirs(rule) {
-	return return rule.toString(2).split("").map(e=>[1,3][e]).reverse();
+	return rule.toString(2).split("").map(e=>[1,3][e]).reverse();
 }
 
-function simulateAnt(rule) {
-	var width = 800;
-	var height = 800;
-	var map = new Array(width*height).fill(0);
+var width, height, map, colors;
+const MAX_ITERATIONS = 200000000;
 
+function simulateAnt(rule) {
+	width = 800;
+	height = 800;
+	map = new Array(width*height).fill(0);
+	
 	var x = width/2;
 	var y = height/2;
 	var dir = 0;
 	var newdir = getDirs(rule);
 	var size = newdir.length;
-
+	
 	const directionx = [0,1,0,-1];
 	const directiony = [-1,0,1,0];
 
-	var colors = new Array(size).fill(0).map(x=>(new Array(3).fill(0).map(y=>Math.floor(Math.random()*256))))
+	colors = new Array(size).fill(0).map(x=>(new Array(3).fill(0).map(y=>Math.floor(Math.random()*256))));
 
 	var startTime = performance.now();
-
-	for(var i = 1; i < 200000000; i++) {
-		var index = x+y*width;
-		var state = map[index];
+	var index = x+y*width;
+	var state;
+	for(var i = 0; i < MAX_ITERATIONS; i+=2) {
+		// HORIZONTAL MOVEMENT
+		state = map[index];
 		dir = (dir+newdir[state])&3;
 		map[index]++;
 		if(map[index]==size) map[index] = 0;
-		//map[index] = newstate[state];
 		x += directionx[dir];
-		y += directiony[dir];
-		if(x<0 || y < 0 || x >= width || y >= height) break;
+		index += directionx[dir];
+		if(x<0 || x >= width) {
+			i++;
+			break;
+		}
+		// VERTICAL MOVEMENT
+                state = map[index];
+                dir = (dir+newdir[state])&3;
+                map[index]++;
+                if(map[index]==size) map[index] = 0;
+                y += directiony[dir];
+                index += directiony[dir]*width;
+		if(y < 0 || y >= height) {
+			i+=2;
+			break;
+		}
 	}
 	console.log(i, "iterations");
 	var endTime = performance.now();
 	console.log(i/(endTime-startTime)*1000,"it/s");
-	render(map,colors,width,height);
+	render();
 }
 
-function render(map,colors,width,height) {
+function render() {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
 
